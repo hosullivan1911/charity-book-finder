9bf118927372b74be9c2c377589d4206f97ac3af
hosullivan1911 <osullivanharry@outlook.com>
2026-08-02 13:06:56 +0800

    Fix Giveleaf search metadata

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   name: "Giveleaf",
+  searchName: "Giveleaf Books",
   supportEmail: "osullivanharry@outlook.com",
-  publicUrl: "https://charity-book-finder.vercel.app",
+  publicUrl: "https://www.giveleafbooks.com",
   scannerUrl: "https://goodfind-shop-scanner.vercel.app",
 };
-
